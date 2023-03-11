436704b8c592822ad53ad5980a9376c88b40972b
sgbus <limdongwan201528@gmail.com>
2023-03-11 22:04:15 +0800

    irrelevant files removed

--- Miscellaneous/Gojek API/mychaelgo/goid-generate-go.sh
+++
@@ -1,4 +0,0 @@
-docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/docs/goid-gojekapi.yaml -g go --additional-properties=packageName=goid_gojek_go -o /local/sdk/goid-gojek-go --git-user-id mychaelgo --git-repo-id gojek/sdk/goid-gojek-go
-cd sdk/goid-gojek-go
-go get -d -v .
-go build -v ./
--- Miscellaneous/Gojek API/mychaelgo/goid-generate-php.sh
+++
@@ -1,3 +0,0 @@
-# https://docs.github.com/en/packages/quickstart
-
-docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/docs/goid-gojekapi.yaml -g php --additional-properties=invokerPackage=GoIdGojekPhp,packageName=GoIdGojekPhp -o /local/sdk/goid-gojek-php
