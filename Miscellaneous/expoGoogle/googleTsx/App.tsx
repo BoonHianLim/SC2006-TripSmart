9d1a0245489fa0b785642163054f1bb165505388
hibye0714 <48339717+YongJian-YJ@users.noreply.github.com>
2023-03-17 16:32:12 +0800

    search page user interface's skeleton there, but function not there

--- Miscellaneous/expoGoogle/googleTsx/App.tsx
+++ Miscellaneous/expoGoogle/googleTsx/App.tsx
@@ -1,48 +1,134 @@
-import React, { useCallback, useMemo, useRef } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import BottomSheet from '@gorhom/bottom-sheet';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import React, { useCallback, useMemo, useRef, useState } from "react";
+import { View, Text, StyleSheet, TextInput } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import { Searchbar } from "react-native-paper";
+import { FontFamily, Color, Margin } from "./GlobalStyles";
+import BottomSheet from "@gorhom/bottom-sheet";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 
 const App = () => {
   // ref
   const bottomSheetRef = useRef<BottomSheet>(null);
 
   // variables
-  const snapPoints = useMemo(() => ['25%', '50%'], []);
+  const snapPoints = useMemo(() => ["25%", "90%"], []);
+
+  // state
+  const [searchQuery, setSearchQuery] = useState("");
+  const [destQuery, setDestQuery] = useState("");
 
   // callbacks
   const handleSheetChanges = useCallback((index: number) => {
-    console.log('handleSheetChanges', index);
+    console.log("handleSheetChanges", index);
   }, []);
+
+  const onChangeSearch = (query: React.SetStateAction<string>) =>
+    setSearchQuery(query);
+
+  const onChangeDest = (query: React.SetStateAction<string>) =>
+    setDestQuery(query);
 
   // renders
   return (
-      <GestureHandlerRootView style={{flex:1}}>
+    <GestureHandlerRootView style={{ flex: 1 }}>
       <View style={styles.container}>
+        <MapView
+          style={{ flex: 1 }}
+          provider={PROVIDER_GOOGLE}
+          region={{
+            latitude: 1.3521,
+            longitude: 103.8198,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}
+          showsUserLocation={true}
+          showsMyLocationButton={true}
+          showsCompass={true}
+          showsScale={true}
+          zoomEnabled={true}
+          rotateEnabled={true}
+          scrollEnabled={true}
+          pitchEnabled={true}
+          toolbarEnabled={true}
+          cacheEnabled={false}
+        />
+
         <BottomSheet
-            ref={bottomSheetRef}
-            index={1}
-            snapPoints={snapPoints}
-            onChange={handleSheetChanges}
+          ref={bottomSheetRef}
+          index={1}
+          snapPoints={snapPoints}
+          onChange={handleSheetChanges}
         >
           <View style={styles.contentContainer}>
-            <Text>Awesome 🎉</Text>
+            <View style={styles.frameContainer}>
+              <View style={styles.textInputContainer}>
+                <Text
+                  style={[styles.email, styles.emailTypo, { marginTop: 10 }]}
+                >
+                  Starting location
+                </Text>
+                <Searchbar
+                  placeholder="Type Here..."
+                  onChangeText={onChangeSearch}
+                  value={searchQuery}
+                  style={{ borderRadius: 5 }}
+                />
+              </View>
+              <View style={styles.textInputContainer}>
+                <Text
+                  style={[styles.email, styles.emailTypo, { marginTop: 10 }]}
+                >
+                  Destination
+                </Text>
+                <Searchbar
+                  placeholder="Type Here..."
+                  onChangeText={onChangeDest}
+                  value={destQuery}
+                  style={{ borderRadius: 5 }}
+                />
+              </View>
+            </View>
           </View>
         </BottomSheet>
       </View>
-      </GestureHandlerRootView>
+    </GestureHandlerRootView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 24,
-    backgroundColor: 'grey',
+    padding: 1,
+    backgroundColor: "grey",
   },
   contentContainer: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: "center",
+  },
+  textInput: {
+    height: 40,
+  },
+  frameContainer: {
+    alignSelf: "stretch",
+  },
+  email: {
+    letterSpacing: 0.3,
+    textAlign: "left",
+    lineHeight: 14,
+    color: Color.textColorsLight,
+    fontSize: 15,
+    alignSelf: "stretch",
+  },
+  emailTypo: {
+    fontFamily: FontFamily.montserratBold,
+    fontWeight: "700",
+  },
+  textInputContainer: {
+    borderColor: "#CCCCCC",
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
   },
 });
 
--- front-end/screens/SearchPage1.tsx
+++ front-end/screens/SearchPage1.tsx
@@ -1,41 +1,135 @@
-import * as React from "react";
-import { Image, StyleSheet, View } from "react-native";
-import HomeSection from "../components/HomeSection";
-import SettingsSection from "../components/SettingsSection";
-import { Color } from "../GlobalStyles";
+import React, { useCallback, useMemo, useRef, useState } from "react";
+import { View, Text, StyleSheet, TextInput } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import { Searchbar } from "react-native-paper";
+import { FontFamily, Color, Margin } from "../GlobalStyles";
+import BottomSheet from "@gorhom/bottom-sheet";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 
-const SearchPage1 = () => {
+const App = () => {
+  // ref
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  // variables
+  const snapPoints = useMemo(() => ["25%", "90%"], []);
+
+  // state
+  const [searchQuery, setSearchQuery] = useState("");
+  const [destQuery, setDestQuery] = useState("");
+
+  // callbacks
+  const handleSheetChanges = useCallback((index: number) => {
+    console.log("handleSheetChanges", index);
+  }, []);
+
+  const onChangeSearch = (query: React.SetStateAction<string>) =>
+    setSearchQuery(query);
+
+  const onChangeDest = (query: React.SetStateAction<string>) =>
+    setDestQuery(query);
+
+  // renders
   return (
-    <View style={styles.searchPage1}>
-      <Image
-        style={styles.screenshot20230208At1155}
-        resizeMode="cover"
-        source={require("../assets/screenshot-20230208-at-1155-1.png")}
-      />
-      <HomeSection />
-      <SettingsSection
-        productId={require("../assets/ticket2.png")}
-        propTop={718}
-        propLeft={15}
-      />
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <MapView
+          style={{ flex: 1 }}
+          provider={PROVIDER_GOOGLE}
+          region={{
+            latitude: 1.3521,
+            longitude: 103.8198,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}
+          showsUserLocation={true}
+          showsMyLocationButton={true}
+          showsCompass={true}
+          showsScale={true}
+          zoomEnabled={true}
+          rotateEnabled={true}
+          scrollEnabled={true}
+          pitchEnabled={true}
+          toolbarEnabled={true}
+          cacheEnabled={false}
+        />
+
+        <BottomSheet
+          ref={bottomSheetRef}
+          index={1}
+          snapPoints={snapPoints}
+          onChange={handleSheetChanges}
+        >
+          <View style={styles.contentContainer}>
+            <View style={styles.frameContainer}>
+              <View style={styles.textInputContainer}>
+                <Text
+                  style={[styles.email, styles.emailTypo, { marginTop: 10 }]}
+                >
+                  Starting location
+                </Text>
+                <Searchbar
+                  placeholder="Type Here..."
+                  onChangeText={onChangeSearch}
+                  value={searchQuery}
+                  style={{ borderRadius: 5 }}
+                />
+              </View>
+              <View style={styles.textInputContainer}>
+                <Text
+                  style={[styles.email, styles.emailTypo, { marginTop: 10 }]}
+                >
+                  Destination
+                </Text>
+                <Searchbar
+                  placeholder="Type Here..."
+                  onChangeText={onChangeDest}
+                  value={destQuery}
+                  style={{ borderRadius: 5 }}
+                />
+              </View>
+            </View>
+          </View>
+        </BottomSheet>
+      </View>
+    </GestureHandlerRootView>
   );
 };
 
 const styles = StyleSheet.create({
-  screenshot20230208At1155: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    width: 565,
-    height: 622,
-  },
-  searchPage1: {
-    backgroundColor: Color.textColorsInverse,
+  container: {
     flex: 1,
-    width: "100%",
-    height: 800,
+    padding: 1,
+    backgroundColor: "grey",
+  },
+  contentContainer: {
+    flex: 1,
+    alignItems: "center",
+  },
+  textInput: {
+    height: 40,
+  },
+  frameContainer: {
+    alignSelf: "stretch",
+  },
+  email: {
+    letterSpacing: 0.3,
+    textAlign: "left",
+    lineHeight: 14,
+    color: Color.textColorsLight,
+    fontSize: 15,
+    alignSelf: "stretch",
+  },
+  emailTypo: {
+    fontFamily: FontFamily.montserratBold,
+    fontWeight: "700",
+  },
+  textInputContainer: {
+    borderColor: "#CCCCCC",
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
   },
 });
 
-export default SearchPage1;
+export default App;
