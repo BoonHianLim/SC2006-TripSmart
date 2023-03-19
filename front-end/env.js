ccb505765bef72d169c5c4d65c56a34a69bbc776
csbhlim222 <csbhlim@gmail.com>
2023-03-19 22:10:22 +0800

    env setup

---
+++ front-end/env.js
@@ -0,0 +1,9 @@
+import {GOOGLE_MAPS_API_KEY} from '@env';
+import {BLUE_SG_API_KEY} from '@env';
+
+const devEnvironmentVariables = {
+    GOOGLE_MAPS_API_KEY: GOOGLE_MAPS_API_KEY,
+    BLUE_SG_API_KEY: BLUE_SG_API_KEY,
+};
+
+export default devEnvironmentVariables;
