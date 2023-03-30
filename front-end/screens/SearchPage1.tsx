696d7374501f62f5f0e76cad818a3daf7f17fbc3
Woon Yee Ng <ngnwy289@gmail.com>
2023-03-30 13:39:25 +0800

    add markers and route tracing

--- front-end/screens/SearchPage1.tsx
+++ front-end/screens/SearchPage1.tsx
@@ -1,7 +1,8 @@
 import React, { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
-import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+import { View, Text, StyleSheet, TextInput, Button, TouchableOpacity } from "react-native";
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import { Marker } from "react-native-maps";
 import { Searchbar } from "react-native-paper";
 import { FontFamily, Color, Margin } from "../GlobalStyles";
 import BottomSheet from "@gorhom/bottom-sheet";
@@ -11,6 +12,37 @@ import Constants from "expo-constants";
 import devEnvironmentVariables from "../env";
 import * as Location from 'expo-location';
 import { LocationGeofencingEventType } from "expo-location";
+import MapViewDirections from "react-native-maps-directions";
+
+type InputAutocompleteProps = {
+  label: string;
+  placeholder?: string;
+  onPlaceSelected: (details: GooglePlaceDetail | null) => void;
+};
+
+function InputAutocomplete({
+  label,
+  placeholder,
+  onPlaceSelected,
+}: InputAutocompleteProps) {
+  return (
+    <>
+      <Text>{label}</Text>
+      <GooglePlacesAutocomplete
+        styles={{ textInput: styles.input }}
+        placeholder={placeholder || ""}
+        fetchDetails
+        onPress={(data, details = null) => {
+          onPlaceSelected(details);
+        }}
+        query={{
+          key: devEnvironmentVariables.GOOGLE_MAP_API_KEY,
+          language: "pt-BR",
+        }}
+      />
+    </>
+  );
+}
 
 const App = () => {
   // ref
@@ -65,30 +97,99 @@ const App = () => {
     text = JSON.stringify(location);
     {/* Example of text: {"timestamp":1680016517725,"mocked":false,"coords":{"altitude":24.899999618530273,"heading":253.0059356689453,"altitudeAccuracy":0.9424006938934326,"latitude":1.3542925,"speed":0.26373162865638733,"longitude":103.6865875,"accuracy":10.979000091552734}} */}
   }
+
+  //markers
+  const [origin, setOrigin] = useState<LatLng | null>();
+  const [destination, setDestination] = useState<LatLng | null>();
+  const [showDirections, setShowDirections] = useState(false);
+  const [distance, setDistance] = useState(0);
+  const [duration, setDuration] = useState(0);
+  const mapRef = useRef<MapView>(null);
+
+  const moveTo = async (position: LatLng) => {
+    const camera = await mapRef.current?.getCamera();
+    if (camera) {
+      camera.center = position;
+      mapRef.current?.animateCamera(camera, { duration: 1000 });
+    }
+  };
+
+  const edgePaddingValue = 70;
+
+  const edgePadding = {
+    top: edgePaddingValue,
+    right: edgePaddingValue,
+    bottom: edgePaddingValue,
+    left: edgePaddingValue,
+  };
+
+  const traceRouteOnReady = (args: any) => {
+    if (args) {
+      // args.distance
+      // args.duration
+      setDistance(args.distance);
+      setDuration(args.duration);
+    }
+  };
+
+  const traceRoute = () => {
+    if (origin && destination) {
+      setShowDirections(true);
+      mapRef.current?.fitToCoordinates([origin, destination], { edgePadding });
+    }
+  };
+
+  const onPlaceSelected = (
+    details: GooglePlaceDetail | null,
+    flag: "origin" | "destination"
+  ) => {
+    const set = flag === "origin" ? setOrigin : setDestination;
+    const position = {
+      latitude: details?.geometry.location.lat || 0,
+      longitude: details?.geometry.location.lng || 0,
+    };
+    set(position);
+    moveTo(position);
+  };
+
   // renders
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <View style={styles.container}>
-        <MapView
-          style={{ flex: 1 }}
-          provider={PROVIDER_GOOGLE}
-          region={{
-            latitude: 1.3521,
-            longitude: 103.8198,
-            latitudeDelta: 0.015,
-            longitudeDelta: 0.0121,
-          }}
-          showsUserLocation={true}
-          showsMyLocationButton={true}
-          showsCompass={true}
-          showsScale={true}
-          zoomEnabled={true}
-          rotateEnabled={true}
-          scrollEnabled={true}
-          pitchEnabled={true}
-          toolbarEnabled={true}
-          cacheEnabled={false}
-        />
+      <MapView
+        style={{ flex: 1 }}
+        provider={PROVIDER_GOOGLE}
+        region={{
+          latitude: 1.3521,
+          longitude: 103.8198,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}
+        showsUserLocation={true}
+        showsMyLocationButton={true}
+        showsCompass={true}
+        showsScale={true}
+        zoomEnabled={true}
+        rotateEnabled={true}
+        scrollEnabled={true}
+        pitchEnabled={true}
+        toolbarEnabled={true}
+        cacheEnabled={false}
+      >
+        {origin && <Marker coordinate={origin} />}
+        {destination && <Marker coordinate={destination} />}
+        {showDirections && origin && destination && (
+          <MapViewDirections
+            origin={origin}
+            destination={destination}
+            apikey={devEnvironmentVariables.GOOGLE_MAP_API_KEY}
+            strokeColor="#6644ff"
+            strokeWidth={4}
+            onReady={traceRouteOnReady}
+          />
+        )}
+      </MapView>
+
         
         <View >
           <Text >{console.log("Your current lattitude is " + latitude + ", current longtitude is " + longitude)}</Text>
@@ -100,48 +201,25 @@ const App = () => {
           snapPoints={snapPoints}
           onChange={handleSheetChanges}
         >
-          <View style={styles.container2}>
-            <GooglePlacesAutocomplete
-              placeholder="Starting location"
-              query={{
-                key: devEnvironmentVariables.GOOGLE_MAP_API_KEY,
-                language: "en", // language of the results
-              }}
-              onPress={(data, details = null) => console.log(data)}
-              onFail={(error) => console.error(error)}
-              requestUrl={{
-                url: "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api",
-                useOnPlatform: "web",
-              }} // this in only required for use on the web. See https://git.io/JflFv more for details.
-              styles={{
-                container: {
-                  flex: 0,
-                },
-              }}
-            />
-
-            <GooglePlacesAutocomplete
-              placeholder="Destination"
-              query={{
-                key: devEnvironmentVariables.GOOGLE_MAP_API_KEY,
-                language: "en", // language of the results
-              }}
-              onPress={(data, details = null) => console.log(data)}
-              onFail={(error) => console.error(error + " Your API Key is: " + devEnvironmentVariables.GOOGLE_MAPS_API_KEY)}
-              requestUrl={{
-                url: "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api",
-                useOnPlatform: "web",
-              }} // this in only required for use on the web. See https://git.io/JflFv more for details.
-              styles={{
-                container: {
-                  flex: 0,
-                },
-              }}
-            />
-            <Button
-              title="Show Result"
-              onPress={() => navigation.navigate("ResultList")}
-            />
+          <View style={styles.searchContainer}>
+          <InputAutocomplete
+            label="Origin"
+            onPlaceSelected={(details) => {
+              onPlaceSelected(details, "origin");
+            }}
+          />
+          <InputAutocomplete
+            label="Destination"
+            onPlaceSelected={(details) => {
+              onPlaceSelected(details, "destination");
+            }}
+          />
+          <TouchableOpacity style={styles.button} onPress={traceRoute}>
+            <Text style={styles.buttonText}>Trace route</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.buttonResult} onPress={() => navigation.navigate("ResultList")}>
+            <Text style={styles.buttonTextResult}>Show Result</Text>
+          </TouchableOpacity>
           </View>
         </BottomSheet>
       </View>
@@ -188,6 +266,45 @@ const styles = StyleSheet.create({
     padding: 10,
     paddingTop: Constants.statusBarHeight + 10,
     backgroundColor: "#ecf0f1",
+  },
+  button: {
+    backgroundColor: "#eeeeee",
+    paddingVertical: 18,
+    marginTop: 16,
+    borderRadius: 4,
+    marginBottom: 16,
+  },
+  buttonResult: {
+    backgroundColor: "blue",
+    color:"white",
+    paddingVertical: 18,
+    marginTop: 16,
+    borderRadius: 4,
+    marginBottom: 16,
+  },
+  buttonText: {
+    textAlign: "center",
+  },
+  buttonTextResult: {
+    textAlign: "center",
+    color:"white",
+  },
+  searchContainer: {
+    position: "absolute",
+    width: "100%",
+    backgroundColor: "white",
+    shadowColor: "black",
+    shadowOffset: { width: 2, height: 2 },
+    shadowOpacity: 0.5,
+    shadowRadius: 4,
+    elevation: 4,
+    padding: 8,
+    borderRadius: 8,
+    top: Constants.statusBarHeight,
+  },
+  input: {
+    borderColor: "#888",
+    borderWidth: 1,
   },
 });
 
