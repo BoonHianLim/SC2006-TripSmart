37b42a040d5f950b8971afbac4586d3562f6aae0
haosheng00 <64892358+haosheng00@users.noreply.github.com>
2023-04-02 21:19:17 +0800

    added fare for pt-api (#10)
    * update getfare from public transport
    
    * update public transport

--- front-end/services/publictransport.tsx
+++ front-end/services/publictransport.tsx
@@ -1,18 +1,69 @@
 import env from "../env";
 import { googlemap } from "./googlemap";
+import React, {useEffect, useState} from "react";
 
 export default class Publictransport{
     private PUBLIC_TRANSPORT_API_KEY: string;
     constructor() {
         if (!env.PUBLIC_TRANSPORT_API_KEY) {
-            throw new Error("TAXI_API_KEY environment variable is not set")
+            throw new Error("PUBLIC_TRANSPORT_API_KEY environment variable is not set")
         }
         this.PUBLIC_TRANSPORT_API_KEY = env.PUBLIC_TRANSPORT_API_KEY || "";
     }
 
     async getData(start:string, end:string): Promise<[number, number]>{
-        // Please complete the code here
+
+        //Call Google Map API
+        const [duration, distance] = await googlemap.getDataString(start, end, "transit");
+        var fare = this.getFare(distance);
+        return [duration, fare];
+
     }
+
+    getFare(distance: number): number{
+        
+        // const [duration, distance] = await googlemap.getDataString(start, end, "transit");
+
+        // const resp = await fetch('https://api.stb.gov.sg/services/transport/v2/mrt-lrt/fare-types', {
+        //     method: 'GET',
+        //     headers: {
+        //         "ApiEndPointTitle" : "Get MRT & LRT Fare Types",
+        //         "X-API-Key" : "IUARFRWUYVcb60dBLafJHCY2SRKMqhdW"
+        //     },
+        //     body: JSON.stringify(query(getStartLat,getStartLng))
+        // });
+        // const json = await resp.json();
+        // console.log(json);
+
+        var fare = 0;
+        if (distance <= 3.2){
+            fare = 1.70;
+        }
+        else if (distance > 3.2 && distance <= 6.2){
+            fare = 1.90;
+        }
+        else if (distance > 6.2 && distance <= 9.2){
+            fare = 2.10;
+        }
+        else if (distance > 9.2 && distance <= 11.2){
+            fare = 2.30;
+        }
+        else if (distance > 11.2 && distance <= 15.2){
+            fare = 2.50;
+        }
+        else if (distance > 15.2 && distance <= 19.2){
+            fare = 2.60;
+        }
+        else if (distance > 19.2 && distance <= 23.2){
+            fare = 2.70;
+        }
+        else{
+            fare = 2.80;
+        }
+    
+        return fare;
+    }
+
 }
 
 export const publictransport = new Publictransport();
