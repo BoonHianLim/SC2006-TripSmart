98ebde3a52303badb60d2b5a2706e44c7293efb1
woonyee28 <ngnwy289@gmail.com>
2023-04-07 23:47:09 +0800

    changing front-end grab server return list

--- front-end/servers/GrabAPI/flaskApp.py
+++ front-end/servers/GrabAPI/flaskApp.py
@@ -29,14 +29,35 @@ def farecheck():
     dropoff_enter = wait.until(EC.presence_of_element_located((By.XPATH, "/html/body/div[4]/div/div/div/ul/li[1]")))
     dropoff_input.send_keys(Keys.ENTER)
 
-    answer = wait.until(EC.presence_of_element_located((By.XPATH,"/html/body/div[2]/div/div/div/div/div/section/div/div/div/div/div/div/div/div/div[2]/div[2]/div/div/div/form/div/div[3]/div[1]/div[3]/span")))
-    
-    fare = answer.get_attribute("innerHTML")
+    firstMode = wait.until(EC.presence_of_element_located((By.XPATH,"/html/body/div[2]/div/div/div/div/div/section/div/div/div/div/div/div/div/div/div[2]/div[2]/div/div/div/form/div/div[3]/div[1]/div[2]/div")))
+    firstFare = wait.until(EC.presence_of_element_located((By.XPATH,"/html/body/div[2]/div/div/div/div/div/section/div/div/div/div/div/div/div/div/div[2]/div[2]/div/div/div/form/div/div[3]/div[1]/div[3]/span")))
+    firstTime = wait.until(EC.presence_of_element_located((By.XPATH,"/html/body/div[2]/div/div/div/div/div/section/div/div/div/div/div/div/div/div/div[2]/div[2]/div/div/div/form/div/div[3]/div[1]/div[2]/small")))
+
+    secondMode = wait.until(EC.presence_of_element_located((By.XPATH,"/html/body/div[2]/div/div/div/div/div/section/div/div/div/div/div/div/div/div/div[2]/div[2]/div/div/div/form/div/div[3]/div[2]/div[2]/div")))
+    secondFare = wait.until(EC.presence_of_element_located((By.XPATH,"/html/body/div[2]/div/div/div/div/div/section/div/div/div/div/div/div/div/div/div[2]/div[2]/div/div/div/form/div/div[3]/div[2]/div[3]/span")))
+    secondTime = wait.until(EC.presence_of_element_located((By.XPATH,"/html/body/div[2]/div/div/div/div/div/section/div/div/div/div/div/div/div/div/div[2]/div[2]/div/div/div/form/div/div[3]/div[2]/div[2]/small")))
+
+    thirdMode = wait.until(EC.presence_of_element_located((By.XPATH,"/html/body/div[2]/div/div/div/div/div/section/div/div/div/div/div/div/div/div/div[2]/div[2]/div/div/div/form/div/div[3]/div[3]/div[2]/div")))
+    thirdFare = wait.until(EC.presence_of_element_located((By.XPATH,"/html/body/div[2]/div/div/div/div/div/section/div/div/div/div/div/div/div/div/div[2]/div[2]/div/div/div/form/div/div[3]/div[3]/div[3]/span")))
+    thirdTime = wait.until(EC.presence_of_element_located((By.XPATH,"/html/body/div[2]/div/div/div/div/div/section/div/div/div/div/div/div/div/div/div[2]/div[2]/div/div/div/form/div/div[3]/div[3]/div[2]/small")))
+
+    ans = []
+    ans.append(firstMode.get_attribute("innerHTML"))
+    ans.append(firstTime.get_attribute("innerHTML"))
+    ans.append(firstFare.get_attribute("innerHTML"))
+
+    ans.append(secondMode.get_attribute("innerHTML"))
+    ans.append(secondTime.get_attribute("innerHTML"))
+    ans.append(secondFare.get_attribute("innerHTML"))
+
+    ans.append(thirdMode.get_attribute("innerHTML"))
+    ans.append(thirdTime.get_attribute("innerHTML"))
+    ans.append(thirdFare.get_attribute("innerHTML"))
     
     driver.quit()
 
     # return fare as a variable
-    return {'fare': fare}
+    return {'fare': ans}
 
 if __name__ == '__main__':
     app.run(debug=True)
