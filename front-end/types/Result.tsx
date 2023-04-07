563b4ff84c0b425792fe3293644b284032531170
csbhlim222 <csbhlim@gmail.com>
2023-04-07 10:19:06 +0800

    fix; fix type

--- front-end/types/Result.tsx
+++ front-end/types/Result.tsx
@@ -1,9 +1,5 @@
 export type Result = {
     name: string;
     iconURL: string;
-<<<<<<< HEAD
     data: Promise<[string, number, number]>[];
-=======
-    data: Promise<[string,number,number]>[];
->>>>>>> c375513 (feat: grab scrapping code + scrapping server code)
 };
